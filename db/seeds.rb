7cb5b81ca996eb9c087ddb1718baadcbc8295ab0
Jordan Patel <logicaljordan247@gmail.com>
2022-07-30 17:07:05 +0200

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  caravan = Caravan.create(
+    name: Faker::Hipster.sentence,
+    address: Faker::Address.street_address,
+    price: rand(10..150),
+    number_of_guests: rand(1..10),
+    description: Faker::Hipster.paragraph,
+    # photo: Faker::LoremFlickr.image(search_terms: ['caravan']),
+    user_id: rand(1..3)
+  )
+  puts "Created #{caravan.name}"
+end
+puts "Finished!"
