9a8aacc7fcef153a9ef908712e6f9304659f0d07
Menakadevi Sundaramoorthy <menakadevisundaramoorthy@gmail.com>
2022-08-02 21:17:59 +0200

    add a review model controller and view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,7 +15,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:comment, :rating)
+    params.require(:review).permit(:content, :rating)
   end
 
   def set_caravan
