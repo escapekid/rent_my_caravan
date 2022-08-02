714ca1eacbc547fda150def69be1f177a3748071
Frank Gerngross <f.gerngross@googlemail.com>
2022-08-02 21:18:48 +0200

    added a map search and address autocomplete

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from '@hotwired/stimulus'
 import mapboxgl from 'mapbox-gl'
-// import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 import "mapbox-gl/dist/mapbox-gl.css"
 // import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css"
@@ -20,6 +20,9 @@ export default class extends Controller {
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
   }
 
   #addMarkersToMap() {
